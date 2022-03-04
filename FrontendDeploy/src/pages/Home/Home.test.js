9d24fbb7be8d46f9d5f1c280221c401005287600
Leonardo Vitor Ferreira Da Silva <53438812894@info.scs>
2022-03-04 16:25:05 -0300

    continuar login

--- FrontendDeploy/src/pages/Home/Home.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from '.';
-
-
-test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/Gerenciamento de Patrimônio/i);
-  expect(linkElement).toBeInTheDocument();
-});
